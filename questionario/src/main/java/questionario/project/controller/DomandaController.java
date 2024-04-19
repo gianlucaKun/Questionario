7d2f9dd3d738753af7a10aab4edf92f949c1ba10
Andriy <Andriy.cascia@gmail.com>
2024-04-19 11:46:24 +0200

    v1

--- questionario/src/main/java/questionario/project/controller/DomandaController.java
+++ questionario/src/main/java/questionario/project/controller/DomandaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +37,7 @@ public class DomandaController {
 		ds.add(d);
 	}
 	
-	@PostMapping("/update")
+	@PutMapping("/update")
 	public DomandaDTO update(@RequestBody DomandaDTO d, @RequestParam("id") Long id) {
 		return ds.update(d, id);
 	}
